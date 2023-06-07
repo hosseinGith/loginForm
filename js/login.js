65c63ad5430c8665652a682f3c4b7e18d5eb74df
hosseinGithu <133234861+hosseinGith@users.noreply.github.com>
2023-06-07 16:38:41 +0330

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,51 +2,70 @@ const container    = document.querySelector('#container')
 const BtnContainer = document.querySelector('#hide')
 const header       = document.querySelector('.header')
 const signUpBtn    = document.querySelector('.signUpLoginBtn')
-const signUptext   = document.querySelector('.signUp')
+const signUptext   = document.querySelector('.svg')
+const loginIcon   = document.querySelector('.login_Icon')
 const textLogin    = document.querySelector('.changer')
-const num          = document.querySelector('#num')
-const email        = document.querySelector('#e-mail')
-const pass         = document.querySelector('#pass')
-const labelpass    = document.querySelector('.email')
+const num          = document.querySelector('.num')
+const email        = document.querySelector('.email')
+const pass         = document.querySelector('.pass')
 
 let trFls = false
 let Login = false
 
 const hossein = {name:'hossein',passd:'123456789'}
 
+//-------------------------------function---------------------------------
+
+function showAlert(){
+    header.style.opacity = '.2'
+    container.style.opacity = '1'
+    container.style.transform = 'translateY(0px)'
+}
+
+
+function hideAlert(){
+    header.style.opacity = '1'
+    container.style.opacity = '0'
+    container.style.transform = 'translateY(-800px)'
+}
+
+
+//-----------------------------arow-function----------------------------
+
 signUptext.addEventListener('click',()=>{
     if(!trFls){
-        signUptext.innerHTML = 'Login'
-        signUpBtn.innerHTML = 'Sign Up'
-        textLogin.innerHTML = 'For Login:'
-        labelpass.classList.add('showB')
+        email.classList.add('showB')
+        signUpBtn.innerHTML = 'sign Up'
+        signUptext.style.display = 'none' 
+        loginIcon.style.display = 'flex'
         Login = true
         trFls = true
-    }else{
-        signUptext.innerHTML = 'Sign Up'
+    }
+}) 
+loginIcon.addEventListener('click',()=>{
+    Login = false
+    trFls = false
         signUpBtn.innerHTML = 'Login'
-        textLogin.innerHTML = 'don`t have the account:'
-        labelpass.classList.remove('showB')
-        Login = false
-        trFls = false
-        }
+        email.classList.remove('showB')
+        signUptext.style.display = 'flex' 
+        loginIcon.style.display = 'none'
 })
 
 signUpBtn.addEventListener('click',()=>{
     
     if(!Login){
-            header.style.opacity = '.2'
-            container.style.opacity = '1'
-            container.style.transform = 'translateY(0px)'
+            showAlert()
     }else{
         
     }
     
    
 })
+window.addEventListener('click',(e)=>{
+    if(e.target ==document.body ){
+    hideAlert()}
 
+})
 BtnContainer.addEventListener('click',()=>{
-    header.style.opacity = '1'
-    container.style.opacity = '0'
-    container.style.transform = 'translateY(-800px)'
+    hideAlert()
 })
